0ac72ed62165d536586d2e5b0e59ea49aa32afb3
Richard Cusimano <rcusimano@ingenio.com>
2023-03-18 14:44:26 -0700

    increased size of vm, open sec group

--- security.tf
+++ security.tf
@@ -43,6 +43,13 @@ resource aws_security_group "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol    = -1
     from_port   = 0
